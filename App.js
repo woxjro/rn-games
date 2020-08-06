284638f19415a754b63fd8dc000296fca1a2eda9
woxjro <n0i6c0k2@gmail.com>
2020-08-07 02:51:11 +0900

    Homeスクリーンを追加

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import {
+    Button,
     View,
     StyleSheet,
     Clipboard,
     TouchableOpacity,
     Text,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import CreateBlock from "./components/create_block";
 import Grid from "./components/grid";
-export default class App extends Component {
+
+const Stack = createStackNavigator();
+
+class TetrisScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -42,6 +48,33 @@ export default class App extends Component {
                     </View>
                 </TouchableOpacity>
             </View>
+        );
+    }
+}
+
+function HomeScreen({ navigation }) {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Tetris"
+                onPress={() => navigation.navigate("Tetris")}
+            />
+        </View>
+    );
+}
+
+export default class App extends Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Tetris" component={TetrisScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
         );
     }
 }
