7261d041c03815bd54cab505153b3790572d304d
woxjro <n0i6c0k2@gmail.com>
2020-08-07 08:12:05 +0900

    不要な記述を削除

--- App.js
+++ App.js
@@ -1,81 +1,10 @@
 import React, { Component } from "react";
-import {
-    Button,
-    View,
-    StyleSheet,
-    Clipboard,
-    TouchableOpacity,
-    Text,
-} from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import TetrisScreen from "./screens/TetrisScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Grid from "./components/grid";
 
 const Stack = createStackNavigator();
-
-export default class TetrisScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            str: "asdfasdf",
-        };
-    }
-    render() {
-        const { navigation } = this.props;
-        return (
-            <View style={styles.container}>
-                <Grid w={10} h={20} />
-                <TouchableOpacity
-                    onLongPress={() => {
-                        Clipboard.setString(this.state.str);
-                    }}
-                >
-                    <View>
-                        <Button
-                            title="Go to Tetris again"
-                            onPress={() => navigation.push("Tetris")}
-                            style={{
-                                color: "red",
-                                fontSize: 14,
-                                fontFamily: "Arial",
-                                fontStyle: "bold",
-                                textAlign: "center",
-                                marginTop: 3,
-                                marginLeft: 25,
-                                marginBottom: 17,
-                            }}
-                        />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-}
-
-export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        const { navigation } = this.props;
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to Tetris"
-                    onPress={() => navigation.push("Tetris")}
-                />
-            </View>
-        );
-    }
-}
 
 export default class App extends Component {
     render() {
@@ -89,11 +18,3 @@ export default class App extends Component {
         );
     }
 }
-
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
