84861d74eb83f5b6ad12f36fe8e119ab91ae4339
woxjro <n0i6c0k2@gmail.com>
2020-08-10 08:42:51 +0900

    fetch部分を実装

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ActivityIndicator, View, Text, FlatList } from "react-native";
 export default class Test extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            data: [],
+            isLoading: true,
+        };
     }
+
+    fetchData = () => {
+        fetch("https://reactnative.dev/movies.json")
+            .then((res) => res.json())
+            .then((json) => this.setState({ data: json.movies }))
+            .catch((error) => console.error(error))
+            .finally(() => this.setState({ isLoading: false }));
+    };
+
+    asyncAwaitFetchData = async () => {
+        try {
+            await this.setState({ isLoading: true });
+            let res = await fetch("https://reactnative.dev/movies.json");
+            let jsn = await res.json();
+            await this.setState({ data: jsn.movies });
+            await this.setState({ isLoading: false });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    componentDidMount() {
+        //this.fetchData();
+        this.asyncAwaitFetchData();
+    }
+
     render() {
         const { navigation } = this.props;
         const { item1, item2 } = this.props.route.params;
-
+        const { isLoading, data } = this.state;
         return (
             <View
                 style={{
@@ -17,6 +46,19 @@ export default class Test extends Component {
                     justifyContent: "center",
                 }}
             >
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <Text>
+                                {item.title}, {item.releaseYear}
+                            </Text>
+                        )}
+                    />
+                )}
                 <Text>Test Screen</Text>
                 <Text>{item1}</Text>
                 <Text>{item2}</Text>
