33d04b3e450c406bbc9c8aee450bb1f78bef7c80
woxjro <n0i6c0k2@gmail.com>
2020-08-09 20:49:13 +0900

    StylePlayGroundスクリーンを作成

---
+++ screens/StylePlayGroudScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export default class StylePlayGroundScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <Text>StyleSheet PlayGround</Text>;
+    }
+}
