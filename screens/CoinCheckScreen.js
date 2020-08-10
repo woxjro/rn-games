f39156ed6d792cf0c4e7c407d6e349d2069e46a7
woxjro <n0i6c0k2@gmail.com>
2020-08-11 00:59:37 +0900

    asksとbidsの平均を出すように。

--- screens/CoinCheckScreen.js
+++ screens/CoinCheckScreen.js
@@ -1,16 +1,65 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 
 export default class CoinCheckScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isLoading: true,
+            orderBooks: {},
+            asksAvg: null,
+            bidsAvg: null,
+        };
+    }
+
+    fetchOrderBooks = async () => {
+        try {
+            await this.setState({ isLoading: true });
+            let res = await fetch("https://coincheck.com/api/order_books");
+            let jsn = await res.json();
+            await console.log(JSON.stringify(jsn));
+            await this.setState({ orderBooks: jsn });
+            await this.calcAsksBidsAvg(this.state.orderBooks);
+            await this.setState({ isLoading: false });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    calcAsksBidsAvg = (orderBooks) => {
+        let asksArray = orderBooks.asks;
+        let bidsArray = orderBooks.bids;
+        let asksSum = 0;
+        let bidsSum = 0;
+
+        asksArray.forEach((ask) => {
+            asksSum += ask[0] * ask[1];
+        });
+        bidsArray.forEach((bid) => {
+            bidsSum += bid[0] * bid[1];
+        });
+        this.setState({
+            asksAvg: asksSum / asksArray.length,
+            bidsAvg: bidsSum / bidsArray.length,
+        });
+    };
+
+    componentDidMount() {
+        this.fetchOrderBooks();
     }
 
     render() {
+        const { isLoading, orderBooks } = this.state;
         return (
             <View>
-                <Text>Coin Check</Text>
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <View>
+                        <Text>asks:{this.state.asksAvg}</Text>
+                        <Text>bids:{this.state.bidsAvg}</Text>
+                    </View>
+                )}
             </View>
         );
     }
