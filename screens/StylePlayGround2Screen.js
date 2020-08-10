d7edf5cfe5224ff677a221fd30f2058b3bef1dc3
woxjro <n0i6c0k2@gmail.com>
2020-08-10 12:05:37 +0900

    オブジェクトの描画をMapで。SettingContainerを作成

--- screens/StylePlayGround2Screen.js
+++ screens/StylePlayGround2Screen.js
@@ -8,33 +8,31 @@ export default class StylePlayGround2Screen extends Component {
     }
 
     render() {
+        const colors = [
+            "aqua",
+            "aquamarine",
+            "dodgerblue",
+            "cornflowerblue",
+            "powderblue",
+            "skyblue",
+            "steelblue",
+        ];
         return (
             <View style={styles.container}>
-                <View
-                    style={{ ...styles.item, backgroundColor: "aqua" }}
-                ></View>
-                <View
-                    style={{ ...styles.item, backgroundColor: "aquamarine" }}
-                ></View>
-                <View
-                    style={{ ...styles.item, backgroundColor: "dodgerblue" }}
-                ></View>
-                <View
-                    style={{
-                        ...styles.item,
-                        backgroundColor: "cornflowerblue",
-                    }}
-                ></View>
-
-                <View
-                    style={{ ...styles.item, backgroundColor: "powderblue" }}
-                ></View>
-                <View
-                    style={{ ...styles.item, backgroundColor: "skyblue" }}
-                ></View>
-                <View
-                    style={{ ...styles.item, backgroundColor: "steelblue" }}
-                ></View>
+                <View style={styles.setting}></View>
+                <View style={styles.itemsContainer}>
+                    {colors.map((color, index) => {
+                        return (
+                            <View
+                                key={index}
+                                style={{
+                                    ...styles.item,
+                                    backgroundColor: color,
+                                }}
+                            ></View>
+                        );
+                    })}
+                </View>
             </View>
         );
     }
@@ -45,7 +43,20 @@ var styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        flexDirection: "row",
+    },
+    setting: {
+        marginTop: 10,
+        marginBottom: 10,
+
+        width: "100%",
+        borderColor: "black",
+        borderWidth: 1,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    itemsContainer: {
+        flex: 2,
     },
     item: {
         width: 40,
