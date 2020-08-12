54cae3a92f3e2cdf43bbb12ca35bba8014e0c521
woxjro <n0i6c0k2@gmail.com>
2020-08-13 00:50:19 +0900

    関数コンポーネントへと書き換え

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,47 +1,40 @@
-import React, { Component } from "react";
+import * as React from "react";
 import { Button, View, Text } from "react-native";
 
-export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        const { navigation } = this.props;
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Button
-                    title="Test"
-                    onPress={() =>
-                        navigation.navigate("Test", {
-                            item1: Math.floor(Math.random() * 100),
-                            item2: "this is item2",
-                        })
-                    }
-                />
-                <Button
-                    title="Coin Check"
-                    onPress={() => navigation.navigate("CoinCheck")}
-                />
-                <Button
-                    title="Tetris"
-                    onPress={() => navigation.navigate("Tetris")}
-                />
-                <Button
-                    title="Maps"
-                    onPress={() => navigation.navigate("GoogleMap")}
-                />
-                <Button
-                    title="StylePlayGround"
-                    onPress={() => navigation.navigate("StylePlayGround")}
-                />
-            </View>
-        );
-    }
+export default function ({ navigation }) {
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Button
+                title="Test"
+                onPress={() =>
+                    navigation.navigate("Test", {
+                        item1: Math.floor(Math.random() * 100),
+                        item2: "this is item2",
+                    })
+                }
+            />
+            <Button
+                title="Coin Check"
+                onPress={() => navigation.navigate("CoinCheck")}
+            />
+            <Button
+                title="Tetris"
+                onPress={() => navigation.navigate("Tetris")}
+            />
+            <Button
+                title="Maps"
+                onPress={() => navigation.navigate("GoogleMap")}
+            />
+            <Button
+                title="StylePlayGround"
+                onPress={() => navigation.navigate("StylePlayGround")}
+            />
+        </View>
+    );
 }
--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
 
-export default class SettingsScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <Text>Settings!</Text>
-            </View>
-        );
-    }
+export default function SettingsScreen({ navigation }) {
+    const [count, setCount] = useState(0);
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Settings!</Text>
+            <Text>now count is {count}</Text>
+            <Button title="+1" onPress={() => setCount(count + 1)} />
+        </View>
+    );
 }
