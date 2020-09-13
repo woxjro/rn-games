f348d94ad5bc209c39df91d0beae599cef301b6e
woxjro <n0i6c0k2@gmail.com>
2020-09-14 00:20:00 +0900

    redux追加

--- redux/redux.js
+++ redux/redux.js
@@ -2,9 +2,6 @@
 import { combineReducers, createStore } from "redux";
 
 // actions.js
-// actionはreduxの機能でなく、オブジェクトを作るための純粋なjsの関数です。
-// 下のcloseKabayaの3行をchromeを開き、command + option + iでコンソールを開き貼り付けましょう。
-// その後、console.log(deleteName())で、{type: "DELETE_NAME", name: ''}というオブジェクトが生成されるのを確かめましょう。
 export const deleteName = () => ({
     type: "DELETE_NAME",
     name: "",
@@ -32,9 +29,6 @@ const COUNT_INITIAL_STATE = {
 };
 
 // reducers.js
-// reduxではglobal stateを巨大なjson(store)として管理します。stateの変更はjsonの書き換えによってのみ管理します。
-// actionは純粋なjsのオブジェクトを作る関数であることを思い出してください。
-// reducerはactionで生成されたオブジェクトを受け取り、巨大なjson(store)を書き換える関数です。
 const userReducer = (state = USER_INITIAL_STATE, action) => {
     switch (action.type) {
         case "SET_NAME":
@@ -63,16 +57,4 @@ export const reducers = combineReducers({
 // store.js
 export const store = createStore(reducers);
 
-// storeは巨大なjsonです。storeの中身を取り出すにはgetStateメソッドを使います。
-// エミュレータでcommand + dを押し、enable remote debugをクリックしましょう。
-// debuggerが現れるので、consoleタブをクリックし、エミュレータ上でアプリをcommandd + rで再起動しましょう。
-console.log("----store.getState()----");
-console.log(store.getState());
-
-// storeはjsonです。つまりjsのオブジェクトです。 jsの関数のtypeofでstoreのstateがオブジェクトであることを確かめましょう。
-console.log("----typeof store.getState----");
-console.log(typeof store.getState);
-
-// storeもまたjsのオブジェクトであり、４つしかメソッドを持たないことを確認しておきましょう。
-console.log("----store----");
-console.log(store);
+console.log("store called!");
--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { View, Text, Button } from "react-native";
-import { plusOne, setName } from "../redux/redux";
-
-function SettingsScreen({ navigation, state, plusOne, setName }) {
+import { plusOne, setName, deleteName } from "../redux/redux";
+import { store } from "../redux/redux";
+function SettingsScreen({
+    navigation,
+    state,
+    counterPlusOne,
+    userSetName,
+    userDeleteName,
+}) {
+    const storeState = store.getState();
     const [count, setCount] = useState(0);
+    const [now, setNow] = useState(new Date());
+    useEffect(() => {
+        setNow(new Date());
+    }, [storeState.user.name]);
     return (
         <View
             style={{
@@ -17,12 +28,26 @@ function SettingsScreen({ navigation, state, plusOne, setName }) {
             <Text>now count is {count} (hooks)</Text>
             <Text>now count is {state.count.count} (redux)</Text>
             <Text>now name is {state.user.name}</Text>
+            <Text>now time is {now.toISOString()}</Text>
             <Button
                 title="+1"
                 onPress={() => {
-                    plusOne(Math.floor(Math.random() * 100));
+                    counterPlusOne(Math.floor(Math.random() * 100));
                     setTimeout(() => setCount(count + 1), 2000);
-                    setName(getRandomString());
+                    console.log("mapStateToProps", state);
+                    console.log("store.getState()", store.getState());
+                }}
+            />
+            <Button
+                title="set name"
+                onPress={() => {
+                    userSetName(getRandomString());
+                }}
+            />
+            <Button
+                title="delete name"
+                onPress={() => {
+                    userDeleteName(state.user.name);
                 }}
             />
         </View>
@@ -46,11 +71,14 @@ const mapStateToProps = (state) => {
 };
 const mapDispatchToProps = (dispatch) => {
     return {
-        plusOne: (count) => {
+        counterPlusOne: (count) => {
             dispatch(plusOne(count));
         },
-        setName: (name) => {
+        userSetName: (name) => {
             dispatch(setName(name));
+        },
+        userDeleteName: (name) => {
+            dispatch(deleteName());
         },
     };
 };
